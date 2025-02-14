051777a847fb3dbc33c755779f022ee62682f548
ET-M1904 <erik.taylor@bsu.edu>
2025-02-14 15:54:07 +0000

    Project progress

---
+++ IO.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+int main(){
+	int num1;
+	int num2;
+	int sum;
+	inFile.open("data.csv");
+	while(getline(inFile, currentLine){
+			getLine(converter, sNum1, ',');
+			getLine(converter, sNum2, ',');
+			getLine(converter, text);
+
+			converter.clear();
+			converter.str("");
+			converter << sNum1 << " " << sNum2:
+			converter >> num1 >> num2
+
+			int sum = num1 + num2;
+			for (int i = sum; i < sum; i++) {
+				
+
+			}
